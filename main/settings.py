02e3c2ddab6c738c3f1af2d44ed07a69d1855697
eshat002 <eshatjubayer22@outlook.com>
2024-05-14 01:42:15 +0600

    done2

--- main/settings.py
+++ main/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#    'default': dj_database_url.parse(config("DATABASE_URL"))
-# }
+DATABASES = {
+   'default': dj_database_url.parse(config("DATABASE_URL"))
+}
 
 
 # Password validation
